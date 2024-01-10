db0db69472ba6528ab36e7f5c90019335987b0bb
Admin <ashiful134@gmail.com>
2024-01-10 20:11:19 +0600

    update

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -15,10 +15,15 @@ func aboutPage(w http.ResponseWriter, r *http.Request) {
 func contactPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page!</h1>")
 }
+
+func faqPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>FAQ page!</h1>")
+}
 func main() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
+	http.HandleFunc("/faq", contactPage)
 	fmt.Println("Starting server on the port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
