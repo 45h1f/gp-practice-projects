2c4545cb713690361e7c59ed0505f24cec884237
Admin <ashiful134@gmail.com>
2024-01-11 21:35:33 +0600

    update

---
+++ gobyexample/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// //hello world
+	// fmt.Println("Hello World")
+
+	// // value
+	// fmt.Println("go" + "lang")
+	// fmt.Println("1+1 =", 1+1)
+	// fmt.Println("7.0/3.0 =", 7.0/3.0)
+	// fmt.Println(true && false)
+	// fmt.Println(true || false)
+	// fmt.Println(!true)
+
+	// //Variables
+	// var a = "initial"
+	// fmt.Println(a)
+	// var b, c int = 1, 2
+	// fmt.Println(b, c)
+	// var d = true
+	// fmt.Println(d)
+	// var e int
+	// fmt.Println(e)
+	// f := "apple"
+	// fmt.Println(f)
+
+	// //Constants
+	// const PI = 3.14
+
+	// fmt.Println(PI)
+
+	// //for condition
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
+	// fmt.Println("-------")
+	// //for  single condition
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+	// fmt.Println("-------")
+	// for {
+	// 	fmt.Println("loop")
+	// 	break
+	// }
+
+	fmt.Println(time.Now().Year())
+
+}
